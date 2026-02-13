abc73203b638d5d090bb5e4f9b6ff379abe0de09
Admin <Admin@DESKTOP-0J3OO3J>
2026-02-13 23:16:46 +0800

    add error message

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -15,4 +15,24 @@ return Application::configure(basePath: dirname(__DIR__))
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
+        $exceptions->render(function (QueryException $e, $request) {
+            return response()->view('errors.custom', [
+                'status' => 500,
+                'message' => 'Database connection problem. Please try again later.'
+            ], 500);
+        });
+
+        $exceptions->render(function (HttpException $e, $request) {
+            return response()->view('errors.custom', [
+                'status' => $e->getStatusCode(),
+                'message' => 'Something went wrong.'
+            ], $e->getStatusCode());
+        });
+
+        $exceptions->render(function (\Throwable $e, $request) {
+            return response()->view('errors.custom', [
+                'status' => 500,
+                'message' => 'Unexpected server error.'
+            ], 500);
+        });
     })->create();
