c8a6dfa4c6c953517dbc968e8efd937b55843301
Admin <Admin@DESKTOP-0J3OO3J>
2026-02-14 00:19:36 +0800

    add route register-disabled

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -20,7 +20,7 @@ class RegisterController extends Controller
      */
     public function showRegistrationForm()
     {
-        return view('pages.register');
+        return view('pages.register-disabled');
     }
 
     /**
